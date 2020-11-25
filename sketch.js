11170198c1bbc1303ab8ece443b3e4c678d6a5ac
auqr <68966281+auqr@users.noreply.github.com>
2020-11-25 16:29:08 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball1;
+
+
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+var ball1_options = {
+	isStatic:false
+}
+
+	ball1 = bodies.circle(width/1.3, 200 , 5 , ball1_options);
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
